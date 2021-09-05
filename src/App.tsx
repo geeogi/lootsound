5001a4458d11fc80865f57728c4c7cc1c3965254
George Bennett <george6bennett@gmail.com>
2021-09-05 16:18:43 +0100

    add sound sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { chestArmour, misc, suffixes, weapons } from "./constants/loot";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           >
             Loot
           </a>
-          . Royalty free.
+          . Royalty free, Creative Commons Licensed.
         </p>
       </header>
       <h2>Bag #7584</h2>
@@ -31,12 +32,68 @@ function App() {
         </div>
       </div>
       <h2>All sounds</h2>
-      <h3>weapons</h3>
-      <h3>chestArmour</h3>
-      <h3>suffixes</h3>
-      <h3>misc.</h3>
+      <div className="Section-sounds">
+        <h3>weapons</h3>
+        <div className="sounds-row">
+          {weapons.map((weapon) => (
+            <div>
+              <p>{weapon}</p>
+              <audio
+                preload="none"
+                src={`/wav/weapons/${weapon}.wav`}
+                controls
+              />
+            </div>
+          ))}
+        </div>
+        <h3>chestArmour</h3>
+        <div className="sounds-row">
+          {chestArmour.map((chestArmour) => (
+            <div>
+              <p>{chestArmour}</p>
+              <audio
+                preload="none"
+                src={`/wav/chestArmour/${chestArmour}.wav`}
+                controls
+              />
+            </div>
+          ))}
+        </div>
+        <h3>suffixes</h3>
+        <div className="sounds-row">
+          {suffixes.map((suffix) => (
+            <div>
+              <p>{suffix}</p>
+              <audio
+                preload="none"
+                src={`/wav/suffixes/${suffix
+                  .replace(" ", "")
+                  .replace("of", "")
+                  .replace("the", "")}.wav`}
+                controls
+              />
+            </div>
+          ))}
+        </div>
+        <h3>misc.</h3>
+        <div className="sounds-row">
+          {misc.map((misc) => (
+            <div>
+              <p>{misc}</p>
+              <audio preload="none" src={`/wav/misc/${misc}.wav`} controls />
+            </div>
+          ))}
+        </div>
+      </div>
       <h2>Download</h2>
+      <a href="/images/myw3schoolsimage.jpg" download>
+        Download zip file of all sounds.
+      </a>
       <h2>License</h2>
+      <p>
+        All sounds have been handpicked from Freesound.org and are Creative
+        Commons Licensed.
+      </p>
     </div>
   );
 }
---
+++ src/constants/loot.ts
@@ -0,0 +1,43 @@
+export const weapons = [
+  "Warhammer",
+  "Quarterstaff",
+  "Maul",
+  "Mace",
+  "Club",
+  "Katana",
+  "Falchion",
+  "Scimitar",
+  "Long Sword",
+  "Short Sword",
+  "Ghost Wand",
+  "Grave Wand",
+  "Bone Wand",
+  "Wand",
+  "Grimoire",
+  "Chronicle",
+  "Tome",
+  "Book",
+];
+
+export const suffixes = [
+  "of Power",
+  "of Giants",
+  "of Titans",
+  "of Skill",
+  "of Perfection",
+  "of Brilliance",
+  "of Enlightenment",
+  "of Protection",
+  "of Anger",
+  "of Rage",
+  "of Fury",
+  "of Vitriol",
+  "of the Fox",
+  "of Detection",
+  "of Reflection",
+  "of the Twins",
+];
+
+export const chestArmour = ["Robe", "Husk", "Shirt", "Mail", "Chestplate"];
+
+export const misc = ["Demon", "Divine", "Dragon", "Holy"];
