ba52ae81127cd760d938d37cc35431142f011281
George Bennett <george6bennett@gmail.com>
2021-09-05 15:49:33 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Loot sound</h1>
+        <p>
+          Sound for{" "}
+          <a
+            href="https://lootproject.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Loot
+          </a>
+          . Royalty free.
+        </p>
+      </header>
+      <h2>Bag #7584</h2>
+      <div className="Section-bag">
+        <div>
+          <div className="Bag">
+            <p>Ring Mail of Giants</p>
+            <p>Demonhide Boots of the Fox</p>
+          </div>
+        </div>
+        <div>
+          <h3>Entrance sound</h3>
+          <h3>Individual sounds</h3>
+        </div>
+      </div>
+      <h2>All sounds</h2>
+      <h3>weapons</h3>
+      <h3>chestArmour</h3>
+      <h3>suffixes</h3>
+      <h3>misc.</h3>
+      <h2>Download</h2>
+      <h2>License</h2>
+    </div>
+  );
+}
+
+export default App;
